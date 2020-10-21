d3d3fe515b72c32d473829d089fa8f3ce1f6b47d
Fernando Iván Sevilla Valderrama <feseva99@gmail.com>
2020-10-21 11:04:59 +0200

    Basic Java Exercises

---
+++ EjerciciosJavaVariables/Ejercicio3JavaVariables.java
@@ -0,0 +1,10 @@
+/*
+* Escribe un programa que muestre tu nombre, dirección y número en lineas separadas.
+*
+* @author Fernando Iván Sevilla Valderrama
+*/
+public class Ejercicio3JavaVariables {
+	public static void main(String[] args) {
+		String nombre 
+	}
+}
